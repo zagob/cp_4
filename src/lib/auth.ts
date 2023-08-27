18f431d45b555330facea5381611b79ed316ce92
zagob <matheusbestana@gmail.com>
2023-08-27 16:08:33 -0300

    ajust type adapter

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,16 +1,18 @@
 import { NextAuthOptions, getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { cert } from "firebase-admin/app";
-import { FirestoreAdapter } from "@next-auth/firebase-adapter";
+// import {  FirebaseAdapterConfig} from "@next-auth/firebase-adapter";
+import { FirestoreAdapter } from "@auth/firebase-adapter";
 
 export const authOptionsFirebase: NextAuthOptions = {
   adapter: FirestoreAdapter({
+    namingStrategy: "snake_case",
     credential: cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
     }),
-  }),
+  }) as {},
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
