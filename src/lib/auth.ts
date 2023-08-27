a8a864d1b1b8aedef6db31f411fa4689b04bd064
zagob <matheusbestana@gmail.com>
2023-08-27 14:57:11 -0300

    remove prisma

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,5 @@
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { NextAuthOptions, getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { db } from "./prisma";
 import { cert } from "firebase-admin/app";
 import { FirestoreAdapter } from "@next-auth/firebase-adapter";
 
@@ -22,13 +20,6 @@ export const authOptionsFirebase: NextAuthOptions = {
 
   callbacks: {
     async session({ token, session, user, newSession }) {
-      // if (token) {
-      //   session.user.id = token.id;
-      //   session.user.name = token.name;
-      //   session.user.email = token.email;
-      //   session.user.image = token.picture;
-      // }
-
       return {
         ...session,
         user: {
@@ -37,58 +28,6 @@ export const authOptionsFirebase: NextAuthOptions = {
         },
       };
     },
-    redirect() {
-      return "/";
-    },
-  },
-};
-
-export const authOptionsPrisma: NextAuthOptions = {
-  adapter: PrismaAdapter(db),
-  session: {
-    strategy: "jwt",
-  },
-  pages: {
-    signIn: "/sign-in",
-  },
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  callbacks: {
-    async session({ token, session }) {
-      if (token) {
-        session.user.id = token.id;
-        session.user.name = token.name;
-        session.user.email = token.email;
-        session.user.image = token.picture;
-      }
-
-      return session;
-    },
-
-    async jwt({ token, user }) {
-      const dbUser = await db.user.findFirst({
-        where: {
-          email: token.email,
-        },
-      });
-
-      if (!dbUser) {
-        token.id = user.id;
-        return token;
-      }
-
-      return {
-        id: dbUser.id,
-        name: dbUser.name,
-        email: dbUser.email,
-        picture: dbUser.image,
-      };
-    },
-
     redirect() {
       return "/";
     },
